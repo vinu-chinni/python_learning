19e3e5df5c1bc55d1e658dd7d06479bf3429b5cb
vinu-chinni <221fa04338@gmail.com>
2026-08-14 20:05:07 +0530

    day4

---
+++ day4.py
@@ -0,0 +1,33 @@
+# strip, lstrip, rstrip methods
+a='coding is simple'
+print(a.strip())
+print(a.lstrip())
+print(a.rstrip())
+#replace
+a='python is simple,python is easy,python is allrounder'
+b=a.replace('python','java')
+print(a)
+print(b)
+#upper, lower, swapcase, title, capitalize
+a='PYTHON is HArd'
+print(a.lower())
+print(a.upper())
+print(a.swapcase())
+print(a.title())
+print(a.capitalize())
+#count, startswith, endswith
+a='abacad'
+b=a.startswith('a')
+c=a.startswith('ad')
+d=a.endswith('d')
+e=a.endswith('de')
+f=a.count('a')
+g=a.count('ad')
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+
